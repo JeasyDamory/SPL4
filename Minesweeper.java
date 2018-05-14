cb9b22b32e42ed0bd5883f5ad9855062884f6b0e
JeasyDamory <171wita30@o365.htl-leoben.at>
2018-05-14 15:31:04 +0200

    Minesweeper, v3

--- Minesweeper.java
+++ Minesweeper.java
@@ -1,19 +1,36 @@
+import javax.swing.*;
+
 public class Minesweeper {
 
     static int _Zeilen  = 3;
     static int _Spalten = 3;
+    static String[][] MinefieldMines = SpielfeldAnlegen(_Zeilen, _Spalten, true);
+    static String[][] MinefieldUser = SpielfeldAnlegen(_Zeilen, _Spalten, false);
+    static int mines = 0;
 
     public static void main(String[] args) {
 
-        String[][] Minefield = SpielfeldAnlegen(_Zeilen,_Spalten);
+        boolean Schleife = true;
 
-        SpielfeldAnzeigen(Minefield, _Zeilen, _Spalten);
+        while(Schleife==true) {
+
+            SpielfeldAnzeigen(MinefieldUser, _Zeilen, _Spalten);
+
+            int eingZeile= eingeben("Bitte Zeile wählen: ");
+            int eingSpalte = eingeben("Bitte Spalte eingeben: ");
+
+            Schleife = SpielfeldPrüfen(eingZeile,eingSpalte);
+
+        }
+
+        System.out.println("GAME OVER");
+
 
 
 
     }
 
-    public static String[][] SpielfeldAnlegen(int Zeilen, int Spalten) {
+    public static String[][] SpielfeldAnlegen(int Zeilen, int Spalten, boolean Minen) {
 
         String[][] Minefield = new String[Zeilen][Spalten];
 
@@ -27,17 +44,20 @@ public class Minesweeper {
 
         }
 
+        if(Minen ==  true) {
+            Minefield[2][0] = "[x]";
+        }
         return Minefield;
     }
 
 
-    public static void SpielfeldAnzeigen(String[][] Minefield, int Zeilen, int Spalten) {
+    public static void SpielfeldAnzeigen(String[][] MinefieldUser, int Zeilen, int Spalten) {
 
         for(int z = 0; z < Zeilen; z++) {
 
             for(int s = 0; s < Spalten; s++) {
 
-                System.out.print(Minefield[z][s]);
+                System.out.print(MinefieldUser[z][s]);
 
             }
 
@@ -45,6 +65,35 @@ public class Minesweeper {
 
         }
 
+        System.out.println("______________");
+
+    }
+
+
+    public static int eingeben(String hinweis) {
+
+        String eingabe = JOptionPane.showInputDialog(hinweis);
+
+        return Integer.parseInt(eingabe);
+
+    }
+
+
+    public static boolean SpielfeldPrüfen(int Zeile, int Spalte) {
+
+        if (MinefieldMines[Zeile][Spalte] == "[x]") {
+
+            System.out.println("Bumm -  du hast eine Mine erwischt!");
+
+            return false;
+        } else {
+
+            System.out.println("Du hast keine Mine erwischt!");
+
+            MinefieldUser[Zeile][Spalte] = "[-]";
+
+            return true;
+        }
 
     }
 
